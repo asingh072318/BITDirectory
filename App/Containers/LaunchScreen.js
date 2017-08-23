28834ad2cb271e05160084c6bd5ac4d146fa5b1f
g4mewarrior <g4mewarrior@Ankits-MacBook-Air.local>
2017-08-23 17:30:11 +0530

    Modal and multiple numbers call done

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { ScrollView, Text, Image, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  Image,
+  View,
+  TouchableOpacity
+} from "react-native";
 import Search from "react-native-search-box";
 import firebase from "firebase";
 import * as FirebaseUtils from "../Services/Firebase";
@@ -7,14 +14,32 @@ import Actions from "jumpstate";
 import { connect } from "react-redux";
 import { List, ListItem, Avatar } from "react-native-elements";
 import Communications from "react-native-communications";
+import Modal from "react-native-modalbox";
+import randomColor from "randomcolor";
 // Styles
 import styles from "./Styles/LaunchScreenStyles";
+const style = StyleSheet.create({
+  modal: {
+    justifyContent: "center"
+  },
+
+  modal3: {
+    height: 200,
+    width: 300
+  },
+
+  text: {
+    color: "black",
+    fontSize: 22
+  }
+});
 class LaunchScreen extends Component {
   constructor(props) {
     super(props);
     FirebaseUtils.initializeFirebase();
     this.state = {
-      search: ""
+      search: "",
+      number: []
     };
   }
   componentWillMount() {
@@ -28,13 +53,19 @@ class LaunchScreen extends Component {
     });
     if (num.length === 1) {
       Communications.phonecall(num[0], false);
+    } else {
+      this.setState({ number: num });
+      this.refs.modal3.open();
     }
+  };
+  call = l => {
+    Communications.phonecall(l, false);
   };
   renderList = () => {
     var contact = this.props.vajra.contact;
     return contact.map((l, i) => {
       if (l.name.toLowerCase().indexOf(this.state.search) !== -1) {
-        let color = "#" + ((Math.random() * 0xffffff) << 0).toString(16);
+        let color = randomColor();
         let abc = (
           <Avatar
             medium
@@ -62,9 +93,39 @@ class LaunchScreen extends Component {
   setSearch = text => {
     this.setState({ search: text.toLowerCase() });
   };
+  renderContent = () => {
+    var num = this.state.number;
+    return num.map((l, i) => {
+      return (
+        <ListItem
+          key={i}
+          title={l}
+          hideChevron
+          leftIcon={{ name: "call" }}
+          titleStyle={{
+            marginLeft: 15,
+            fontSize: 17
+          }}
+          onPress={() => this.call(l)}
+        />
+      );
+    });
+  };
   render() {
     return (
       <View style={{ backgroundColor: "white", flex: 1 }}>
+        <Modal
+          style={[style.modal, style.modal3]}
+          position={"center"}
+          ref={"modal3"}
+        >
+          <View style={{ flex: 1, justifyContent: "flex-end", padding: 15 }}>
+            <Text style={style.text}>Choose Number </Text>
+          </View>
+          <View style={{ flex: 3 }}>
+            {this.renderContent()}
+          </View>
+        </Modal>
         <View
           style={{
             flex: 1,
